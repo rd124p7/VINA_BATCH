ffa483ea52d5909e0eccb28be395e5d50aefaef0
rd124p7 <rd124p7@yahoo.com>
2018-02-10 13:05:35 -0500

    Added FindProteinPath and FindStructuresPath to the Operations class

---
+++ VINA_BATCH/FFManagement/Operations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace VINA_BATCH.FFManagement
+{
+    class Operations
+    {
+        private string structuresPath = Path.Combine(Directory.GetCurrentDirectory(), "structures");
+        private static string workingPath = Path.Combine(Directory.GetCurrentDirectory(), "working");
+        private string structureExt = ".pdbqt";
+
+        public Operations()
+        {
+
+        }
+
+        public string FindProteinPath()
+        {
+            string[] workingFiles = Directory.GetFiles(workingPath, structureExt);
+            return workingFiles[0];
+        }
+
+        public string[] FindStructuresPath()
+        {
+            return Directory.GetFiles(structuresPath, structureExt);
+        }
+
+    }
+}
